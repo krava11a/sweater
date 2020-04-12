56866b82bdbb665d8d9aec050e0a03342c30dc92
krava <boga11a@yandex.ru>
2020-04-12 12:23:54 +0300

    9 lesson. Add user's mail verification

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -1,43 +1,54 @@
 package com.example.sweater.controller;
 
 
-import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
-import com.example.sweater.repos.UserRepo;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo repo;
+    private UserService repo;
 
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user,Map<String,Object> model){
+    public String addUser(User user, Map<String, Object> model) {
 
-        User userFromDB = repo.findByUsername(user.getUsername());
-        if (userFromDB != null){
+
+        if (!repo.addUser(user)) {
             model.put("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        repo.save(user);
 
         return "redirect:/login";
 
     }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = repo.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message","User successfully activated!");
+        }else{
+            model.addAttribute("message","Activation code is not found!");
+
+        }
+
+        return "login";
+    }
+
 }
--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.sweater.service;
 
+import antlr.StringUtils;
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,11 +10,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.UUID;
+
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
     private final UserRepo userRepo;
+
+    @Autowired
+    private MailSender mailSender;
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
@@ -20,5 +29,47 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDB = userRepo.findByUsername(user.getUsername());
+
+
+        if(userFromDB != null){
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepo.save(user);
+
+        if (!org.springframework.util.StringUtils.isEmpty(user.getEmail())){
+
+            String message = String.format(
+                    "Hello, %s! \n"+
+                            "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(),"Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if (user == null){
+            return false;
+        }
+
+        user.setActivationCode(null);
+
+        userRepo.save(user);
+
+        return true;
     }
 }
